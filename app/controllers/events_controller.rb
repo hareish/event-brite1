08ab44d73d3fde4a0ad3bd53cf87dfd412852333
hareish <hareish3@gmail.com>
2021-05-18 18:49:22 +0200

    update landing page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,53 +1,66 @@
 class EventsController < ApplicationController
-    def index 
-     @event = Event.all
-    end 
+  before_action :set_event, only: [:show, :edit, :update, :destroy]
 
 
-    def show 
-        @event =Event.find(params[:id])
-        @admin =User.find(@event.admin_id)
-        @nb_guests = attendance.where(even_id: @event.id).count
-    end 
+  def index
+    @events = Event.all
+  
+  end
 
-    def new 
-        @event =Event.new 
-    end 
-
-    def create 
-        @event = Event.new (title:params [:Title], 
-        description: params [:description], 
-        prince: params[:price],
-        start_date: params [:start_date],
-        duration: params [:duration], 
-        admin_id: current_user_id)
-
-        if @event.save 
-            flash [:success] = "tu as réussi a créer l'évenement"
-            redirect_to :controller => 'event', :action => 'show', id: @event.id 
-        else 
-            flash.now [:danger] ="erruer avec la creation de votre compte"
-        end 
-    end 
-    
-    def update 
-        respond_to do |format|
-            if @event.update (event_params)
-                format.html { redirect_to @event; notice : 'événement a été bien actualiser'}
-                format.json { redirect_to : show, status: ok, location @event}
-            else
-                format.html { render :edit}
-                format .json 
+  
+  def show
+    @event = Event.find(params[:id])
+    @admin = User.find(@event.admin_id)
+    @nb_guests = Attendance.where(event_id: @event.id).count
+  end
 
 
-    def destroy 
-        @event.destroy 
-        respond_to do |format|
-            format.html { redirect_to event_url, notice: 'évenement a été bien supprimer'}
-            format.json { head: no_content}
-        end 
-    end 
+  def new
+    @event = Event.new
+  end
 
 
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+
+  def create
+    @event = Event.new(title: params[:title], 
+      description: params[:description],
+      location: params[:location],
+      price: params[:price],
+      start_date: params[:start_date],
+      duration: params[:duration],
+      admin_id: current_user.id)
+
+    if @event.save 
+        flash[:success] = "votre evenement a été bien créer"
+        redirect_to :controller => 'events', :action => 'show', id: @event.id
+    else
+ 
+      flash.now[:danger] = "erreur avec création de compte"
+      render :action => 'new'
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @event.update(event_params)
+        format.html { redirect_to @event, notice: 'votre évenement a été bien créer' }
+  
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  def destroy
+    @event.destroy
+    respond_to do |format|
+      format.html { redirect_to events_url, notice: 'votre événement a été bien supprimer' }
+
+    end
+  end
 
 end
--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,5 @@
 module EventsHelper
+     def get_end_date(event)
+    @event.start_date + @event.duration*60
+  end
 end
